3d0959b907cb703814d9f21dc99610539f1e0f34
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-10-16 21:21:55 +0100

    Working example, fetching images and stretching to fit screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 import "./App.css";
 
 const App = ({ username }) => {
+    const [images, setImages] = useState(null);
+    const [imageDimensions, setImageDimensions] = useState(0);
+
+    // Upon the initial load go and grab the images from instagram
     useEffect(() => {
         async function getImages() {
-            if (!username) return;
             const res = await axios.get(
                 `https://www.instagram.com/${username}/?__a=1`
             );
@@ -15,14 +19,62 @@ const App = ({ username }) => {
         }
 
         getImages();
+        // eslint-disable-next-line
     }, []);
 
-    const [images, setImages] = useState(null);
+    //Set the image dimensions (to stretch the screen)
+    function getImageDimensions() {
+        if (!images) {
+            setImageDimensions(0);
+        } else {
+            setImageDimensions(
+                Math.sqrt(
+                    (window.innerHeight * window.innerWidth) / images.length
+                )
+            );
+            // Testing purposes for the moment
+            console.log(
+                Math.sqrt(
+                    (window.innerHeight * window.innerWidth) / images.length
+                )
+            );
+        }
+    }
+
+    // get the correct dimensions for the images,
+    //note we've added an event listener to watch for a screen resize
+    useEffect(getImageDimensions, [images]);
+    window.addEventListener("resize", getImageDimensions);
+
+    const Container = styled.div`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-wrap: wrap;
+        position: fixed;
+    `;
+
+    const Panel = styled.div`
+        height: ${imageDimensions}px;
+        width: ${imageDimensions}px;
+        flex-grow: 1;
+    `;
+
+    const Item = styled.img`
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+    `;
+
     return (
-        <div className="App">
+        <Container>
             {images &&
-                images.map(res => <img src={res[1].src} alt="recent post" />)}
-        </div>
+                images.map(res => (
+                    <Panel key={res[1].src}>
+                        <Item src={res[1].src} alt="recent post" />
+                    </Panel>
+                ))}
+        </Container>
     );
 };
 
